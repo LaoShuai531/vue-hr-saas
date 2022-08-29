d3387641595a82d1a6ac8d58dc51d26abab48bf4
Vous <1604059791@qq.com>
2022-08-29 14:10:47 +0800

    '项目初始化'

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+// setting.js其实是对于一些项目信息的配置
+module.exports = {
+
+  title: '人资后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo显示
+  sidebarLogo: false
+}
